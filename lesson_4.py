fa3d636c0b87608c6b737bf056b7e20c1d235e42
andrey-tar <andrendiy@gmail.com>
2024-11-30 13:59:31 +0100

    little tweaks

--- lesson_4.py
+++ lesson_4.py
@@ -1,4 +1,6 @@
-#task 1.1
+# Створіть список чисел. Додайте до списку числа 10 і 20,
+# видаліть число 10 і виведіть отриманий список.
+
 first_list = [1,2,3,4,5]
 first_list.append(10)
 first_list.append(20)
@@ -7,7 +9,8 @@ first_list.remove(10)
 print(first_list)
 print()
 
-#task 1.2
+# Створіть список чисел. Знайдіть та виведіть суму всіх чисел у списку.
+
 second_list = [1,2,3,4,5.9,10,929]
 summ = 0
 index = 0
@@ -18,7 +21,8 @@ for items in second_list:
 print(summ)
 print()
 
-#task 1.3
+# Створіть список чисел. Подвойте кожне число у списку та виведіть результат.
+
 third_list = [1,2,3,4,5.9,10,929]
 index = 0
 third_list_2=[]
@@ -29,20 +33,28 @@ for items in third_list:
 print(third_list_2)
 print()
 
-#task 2.1
+# Створіть кортеж з трьох різних предметів, таких як
+# ("яблуко", "банан", "апельсин"). Виведіть кожен елемент
+# кортежу окремо.
+
 word_tuple = ('apple','banana','orange')
 print(word_tuple[0])
 print(word_tuple[1])
 print(word_tuple[2])
 print()
 
-#task 2.2
+# Створіть два кортежі з числами і об'єднайте їх у новий кортеж.
+# Виведіть отриманий кортеж.
+
 tuple1 = (1,2,3)
 tuple2 = (4,5,6)
 tuple3 = tuple1 + tuple2
 print(tuple3)
 
-#task 3.1
+# Створіть словник, що містить інформацію про вашого
+# улюбленого спортсмена (ім'я, вік, спорт, команда тощо).
+# Виведіть цю інформацію на екран.
+
 sport_dict = {'name':'Sergei Rebrov',
               'age':'50',
               'sport':'football',
@@ -50,7 +62,10 @@ sport_dict = {'name':'Sergei Rebrov',
 print(sport_dict)
 print()
 
-#task 3.2
+# Створіть словник, що містить ваші улюблені книги
+# (назва книги та рік видання). Додайте до словника
+# нову улюблену книгу та виведіть оновлений словник.
+
 book_dict = {'Dark Tower':'1982',
              'Slaughterhouse-Five':'1969',
              'Ham on Rye':'1982'}
@@ -58,7 +73,10 @@ book_dict.update({"The Chronicles of Amber": '1970'})
 print(book_dict)
 print()
 
-#test 3.3
+# Створіть словник, що містить інформацію про країни та
+# їх столиці. Запитайте користувача про назву країни і
+# виведіть столицю цієї країни (якщо така країна є у словнику).
+
 cap_dict = {'Ukraine':'Kyiv',
             'Russia':'Moscow',
             'USA':'Washington'}
